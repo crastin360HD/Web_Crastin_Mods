e715ed3a197d715bcb384cd2a566720fa3962d60
Crastin <89280408+crastin360HD@users.noreply.github.com>
2021-10-22 14:36:01 -0500

    Cambio_13

--- menu_servicio.js
+++ menu_servicio.js
@@ -1,13 +1,8 @@
 
-let open_descargas = document.querySelector('.boton_descarga');
-let link_descargas = document.querySelector('.link_descargas');
-let cerrar_descargas = document.querySelector('.show_descargas');
+let open_descargas = document.querySelector(".icon_open");
+let link_relacion = document.querySelector(".links-relacionados");
+let abrir_cerrar = document.querySelector(".show_descargas");
 
-
-open_descargas.addEventListener('click', ()=>{
-    link_descargas.classList.toggle("show_descargas")
+open_descargas.addEventListener('click',()=>{
+    link_relacion.classList.toggle("show_descargas");
 })
-
-console.log(open_descargas)
-console.log(link_descargas)
-console.log(cerrar_descargas)
