b156850026e60b878bf3b7b30740b696f0f06dbb
Crastin <89280408+crastin360HD@users.noreply.github.com>
2021-10-23 10:27:28 -0500

    Cambio_14

--- menu-2.js
+++ menu-2.js
@@ -16,3 +16,15 @@ window.addEventListener('click', e=>{
 
     }
 })
+
+
+// * Desplazamiento pc movil (PC) //
+
+let Icon_Menu_desplegable = document.querySelector(".Icon_desplegable_movil");
+let Menu_desplegable = document.querySelector(".menu-movil");
+let abrir_cerrar_menu = document.querySelector(".show-menu-movil");
+
+
+Icon_Menu_desplegable.addEventListener('click', ()=>{
+    Menu_desplegable.classList.toggle("show-menu-movil")
+})
--- menu_servicio.js
+++ menu_servicio.js
@@ -1,4 +1,3 @@
-
 let open_descargas = document.querySelector(".icon_open");
 let link_relacion = document.querySelector(".links-relacionados");
 let abrir_cerrar = document.querySelector(".show_descargas");
@@ -6,3 +5,5 @@ let abrir_cerrar = document.querySelector(".show_descargas");
 open_descargas.addEventListener('click',()=>{
     link_relacion.classList.toggle("show_descargas");
 })
+
+
