a9b36549a672590d74c0f5192b924303254fcfcb
Crastin <89280408+crastin360HD@users.noreply.github.com>
2021-11-04 12:40:24 -0500

    Update_relative

--- menu_servicio.js
+++
@@ -1,9 +0,0 @@
-let open_descargas = document.querySelector(".icon_open");
-let link_relacion = document.querySelector(".links-relacionados");
-let abrir_cerrar = document.querySelector(".show_descargas");
-
-open_descargas.addEventListener('click',()=>{
-    link_relacion.classList.toggle("show_descargas");
-})
-
-
