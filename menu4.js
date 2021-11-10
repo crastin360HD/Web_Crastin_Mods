91a9c9ac91fc13278cdc0e90be0ff15cb62bd5fb
Crastin <89280408+crastin360HD@users.noreply.github.com>
2021-11-10 14:06:20 -0500

    Update_theme

--- menu4.js
+++ menu4.js
@@ -9,3 +9,32 @@ icon_menu.addEventListener('click', ()=>{
 icon_cerrar.addEventListener('click', ()=>{
     menu.classList.remove("show__menu")
 })
+
+
+
+window.addEventListener('click', e=>{
+    
+    if(menu.classList.contains('show__menu')
+        &&e.target != menu &&e.target != icon_menu  ){
+        
+
+        menu.classList.toggle("show__menu")
+        
+    }
+})
+
+
+//**Move__content*/
+
+/*const cars__information = document.querySelector(".new__cars__information");
+const image__information = document.querySelector(".image_new_cars");
+const block__conts = document.querySelector('.block__content');
+
+block__conts.addEventListener('click', ()=>{
+    cars__information.classList.toggle('cars__show');
+    
+})
+
+block__conts.addEventListener('click',()=>{
+    image__information.classList.toggle('image__show')
+})*/
